8d0cd7b18ea2484cd9559f1f13c9df83e9df25b6
BernardoSemiOficial <bernardopereiraoliveira258@gmail.com>
2023-10-14 13:33:50 -0300

    chore: configure form control

--- challenger-smartfit/src/app/app.module.ts
+++ challenger-smartfit/src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContentTitleComponent } from './components/content-title/content-title.component';
-import { HeaderComponent } from './components/header/header.component';
 import { FormComponent } from './components/form/form.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-	declarations: [AppComponent, HeaderComponent, ContentTitleComponent, FormComponent],
-	imports: [BrowserModule, AppRoutingModule],
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		ContentTitleComponent,
+		FormComponent,
+	],
+	imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
--- challenger-smartfit/src/app/components/form/form.component.ts
+++ challenger-smartfit/src/app/components/form/form.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+	FormBuilder,
+	FormControl,
+	FormGroup,
+	Validators,
+} from '@angular/forms';
+
+interface GymFilter {
+	period: FormControl<string | null>;
+	closedUnits: FormControl<boolean | null>;
+}
 
 @Component({
 	selector: 'app-form',
 	templateUrl: './form.component.html',
 	styleUrls: ['./form.component.scss'],
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
 	unitsFound = 0;
+	gymFilter!: FormGroup<GymFilter>;
+
+	constructor(private formBuilder: FormBuilder) {}
+
+	/** MÉTODO QUE EXECUTA NA PRIMEIRA RENDERIZAÇÃO DO COMPONENTE */
+	ngOnInit() {
+		this.gymFilter = this.formBuilder.group<GymFilter>({
+			period: new FormControl('', Validators.required),
+			closedUnits: new FormControl(false, Validators.required),
+		});
+	}
+
+	onSubmit() {
+		console.log('onSubmit', this.gymFilter.value);
+	}
+
+	onClear() {
+		this.gymFilter.reset();
+		console.log('onClear', this.gymFilter.value);
+	}
 }
