d1f6995bfcfbf0585fe769ae13a81e114095f2c1
Bernardo Pereira <bernardopereiraoliveira258@gmail.com>
2023-10-25 23:01:07 -0300

    feat: update card-list and card-item

--- challenger-smartfit/src/app/components/card/card.component.ts
+++ challenger-smartfit/src/app/components/card/card.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Location } from '~/app/types/gym-locations.interface';
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+	selector: 'app-card',
+	templateUrl: './card.component.html',
+	styleUrls: ['./card.component.scss'],
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+	@Input() card!: Location;
 
+	ngOnInit(): void {
+		console.log('card', this.card);
+	}
 }
